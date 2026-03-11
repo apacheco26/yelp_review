c0441e0ea9fa4462aab1235a3d74365a269f5f82
Julian Pacheco <apacheco26@outlook.com>
2026-03-11 12:36:51 -0700

    fixed create_tables to create_table()

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fetcher import fetch_all
-from db import create_tables, save_reviews
+from db import create_table, save_reviews
 from dotenv import load_dotenv
 load_dotenv()
+import os, sys
+
+print("Starting main.py")
+print("API key present?", os.getenv("YELP_API_KEY") is not None, file=sys.stderr)
 
 def main():
     print("Starting data fetching process...")
 
     # creeate the database tables if they don't exist
-    create_tables()
+    create_table()
 
     reviews = fetch_all()
     
